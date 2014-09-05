e9c5d7e060e30b69ec7ce04229342abf1b6d2864
Alexander Mendez <bluespace@gmail.com>
2014-09-05 14:44:30 -0400

    emacs fun

--- dotfiles/.emacs.d/ajmendez.el
+++ dotfiles/.emacs.d/ajmendez.el
@@ -52,7 +52,7 @@
 
 ;; Load some paths: ------------------------------------------------------------
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-        (let* ((my-lisp-dir (expand-file-name "~/Dropbox/etc/emacs/site-lisp"))
+        (let* ((my-lisp-dir (expand-file-name "~/.emacs.d/site-lisp"))
               (default-directory my-lisp-dir))
            (setq load-path (cons my-lisp-dir load-path))
            (normal-top-level-add-subdirs-to-load-path)))
